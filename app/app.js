0c6fef9c40060342096852fe9892ae19790e4460
Tony Meyer <mediamaker@gmail.com>
2013-10-10 17:08:00 +0200

    first commit

---
+++ app/app.js
@@ -0,0 +1,21 @@
+angular.module('chat', ['firebase'])
+  .controller('Chat', ['$scope', '$timeout', 'angularFireCollection',
+    function($scope, $timeout, angularFireCollection) {
+      var url = 'https://realtimechat.firebaseio.com/';
+      $scope.messages = angularFireCollection(new Firebase(url).limit(20));
+      $scope.username = 'Guest' + Math.floor(Math.random()*101);
+      $scope.addMessage = function() {
+        $scope.messages.add({from: $scope.username, content: $scope.message});
+        $scope.message = "";
+      }
+    }
+  ])
+  .directive('autoScroll', function($timeout) {
+    return function(scope, elements, attrs) {
+      scope.$watch("messages.length", function() {
+        $timeout(function() {
+          elements[0].scrollTop = elements[0].scrollHeight
+        });
+      });
+    }
+  });
